242a6ff811ff4b2d2589c260ab5f84579d944269
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-10-04 20:51:23 -0300

    Estrutura basica de pastas

--- src/routes.ts
+++ src/routes.ts
@@ -7,12 +7,18 @@ const userController = new UserController();
 const routes = express.Router();
 
 
-routes.get('/', (req,res)=>{
-    res.json({
-        "name": "javascript_typescrip_knex_structure",
-        "repository": "https://github.com/anisioaleixo/javascript_typescrip_knex_structure.git",
-        "author": "Anisio Aleixo",
+routes
+    //Initi da aplicação
+    .get('/', (req,res)=>{
+        res.json({
+            "name": "javascript_typescrip_knex_structure",
+            "repository": "https://github.com/anisioaleixo/javascript_typescrip_knex_structure.git",
+            "author": "Anisio Aleixo",
+        })
     })
-});
+    //.get('/users',userController.index)
+    //.post('/users',userController.create)
+    //.delete('/users/:id',userController.delete)
+    //.put('/users/:id',userController.update)
 
 export default routes;
